18bf540d1cab2843287323c173e89dc3d27e5bdd
Hault03 <hault2003@gmail.com>
2025-02-27 15:02:53 +0700

    setup project

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
+const moduleResolver = [
+    "module-resolver",
+    {
+        root: ["./"],
+        extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+        alias: {
+            "@": "./src",
+        },
+    },
+]
+
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-};
+    presets: ["module:@react-native/babel-preset"],
+    plugins: [moduleResolver],
+}
--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,21 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-
+const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config")
+const sourceExts = require("metro-config/src/defaults/defaults").sourceExts
+const assetExts = require("metro-config/src/defaults/defaults").assetExts
 /**
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const config = {
+    transformer: {
+        babelTransformerPath: require.resolve("react-native-svg-transformer"),
+    },
+    resolver: {
+        assetExts: assetExts.filter((ext) => ext !== "svg"),
+        sourceExts: [...sourceExts, "svg"],
+    },
+}
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config)
