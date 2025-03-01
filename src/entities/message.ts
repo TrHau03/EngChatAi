fbba0c2a3291f506d969763190bef7d511cc158b
Hault03 <hault2003@gmail.com>
2025-03-01 14:50:29 +0700

    feat message item

---
+++ src/entities/message.ts
@@ -0,0 +1,4 @@
+export interface Message {
+    isOwner: boolean
+    content: any
+}
--- src/screens/Chat/NewChat.tsx
+++ src/screens/Chat/NewChat.tsx
@@ -1,19 +1,42 @@
-import { Wrapper } from "@/components"
+import { MessageItem, Wrapper } from "@/components"
 import InputBar from "@/components/InputBar"
 import { NewChatProps } from "@/navigation/stack/RootStack"
 import { spacing } from "@/theme"
 import { useNavigation } from "@react-navigation/native"
 import { makeStyles } from "@rneui/themed"
-import React from "react"
-import { View } from "react-native"
+import React, { useCallback } from "react"
+import { FlatList, KeyboardAvoidingView, Platform } from "react-native"
 
 const NewChat = () => {
     const styles = useStyles()
     const navigation = useNavigation<NewChatProps>()
+    const behavior = Platform.OS === "ios" ? "padding" : "height"
+    const data = [
+        {
+            isOwner: true,
+            content: "Hello",
+        },
+        {
+            isOwner: false,
+            content: "Hello",
+        },
+        {
+            isOwner: true,
+            content: "Hello",
+        },
+    ]
+
+    const renderItem = useCallback(({ item }: any) => {
+        return <MessageItem isOwner={item.isOwner} content={item.content} />
+    }, [])
+
     return (
         <Wrapper isSafeArea containerStyle={styles.container}>
-            <View style={{ flex: 1 }}></View>
-            <InputBar />
+            <KeyboardAvoidingView style={{ flex: 1 }} behavior={behavior}>
+                {/* <View style={{ flex: 1 }}></View> */}
+                <FlatList data={data} renderItem={renderItem} />
+                <InputBar />
+            </KeyboardAvoidingView>
         </Wrapper>
     )
 }
