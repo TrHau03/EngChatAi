6d1759d8af8e29d11d455bbe17747ea075e822d5
vulg3 <100254440+vulg3@users.noreply.github.com>
2025-03-04 00:48:51 +0700

    update screen settings

---
+++ src/screens/Settings/SettingsDetailScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SettingsDetailScreen = () => {
+  return (
+    <View>
+      <Text>SettingsDetailScreen</Text>
+    </View>
+  )
+}
+
+export default SettingsDetailScreen
+
+const styles = StyleSheet.create({})
